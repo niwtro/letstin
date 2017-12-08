e4c9ef4d7f36311508a7371930a3ed94e798968f
Stefan Esterer <stefan.esterer@gmail.com>
2017-12-08 17:10:05 +0100

    startable spring boot application

---
+++ backend/src/main/java/org/letstin/LetstinApplication.java
@@ -0,0 +1,14 @@
+package org.letstin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LetstinApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LetstinApplication.class, args);
+    }
+
+
+}
